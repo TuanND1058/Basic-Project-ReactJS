08479d4fc39423871eb920a3edec792a99796ac4
TuanND <tuannd1058@gmail.com>
2023-02-01 19:55:26 +0700

    12 sidebar modal

--- 12-sidebar-modal/src/index.js
+++ 12-sidebar-modal/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { AppProvider } from './context'
+import App from './App'
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
