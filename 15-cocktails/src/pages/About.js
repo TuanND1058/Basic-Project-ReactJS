af1e5de71643439dc5b289006f92755fc85c4260
TuanND <tuannd1058@gmail.com>
2023-02-07 20:03:42 +0700

    Complete 15 cocktails

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam veniam qui reprehenderit deleniti! Laudantium
+        ipsam blanditiis sunt sequi unde, eos beatae totam aut vel reprehenderit incidunt officia neque eveniet
+        excepturi velit alias expedita quasi vero delectus officiis ducimus dolorum consequatur.
+      </p>
+    </section>
   )
 }
 
